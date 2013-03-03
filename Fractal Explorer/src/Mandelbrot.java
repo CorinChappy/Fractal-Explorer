645d7db6fc858d159d803c2baf07f09736c46203
Corin Chaplin <corindc@gmail.com>
2013-03-03 01:22:42 +0000

    Resizing bug now fixed. The range representations are now defined by R and I instead of X and Y

--- Fractal Explorer/src/Mandelbrot.java
+++ Fractal Explorer/src/Mandelbrot.java
@@ -9,19 +9,19 @@ import javax.swing.JPanel;
 public class Mandelbrot extends JPanel{
 	
 	// ints to represent the ranges
-	private double minX;
-	private double maxX;
-	private double minY;
-	private double maxY;
+	private double minR;
+	private double maxR;
+	private double minI;
+	private double maxI;
 	
-	public final static int ITTERATIONS = 100;
+	public final static int ITTERATIONS = 10000;
 	
 	public Mandelbrot(){
 		// Set the min & max
-		minX = -2;
-		maxX = 2;
-		minY = -1.6;
-		maxY = 1.6;
+		minR = -2;
+		maxR = 2;
+		minI = -1.6;
+		maxI = 1.6;
 		
 		this.addMouseListener(new Listener());
 		this.setBackground(Color.WHITE);
@@ -33,20 +33,20 @@ public class Mandelbrot extends JPanel{
 	protected void paintComponent(Graphics g){
 		super.paintComponent(g);
 		// Paint each pixel with the x axis then the y axis
-		for(int Y=0; Y<=this.getWidth(); Y++){
-			for(int X=0; X<=this.getHeight(); X++){
+		for(int Y=0; Y<=this.getHeight(); Y++){
+			for(int X=0; X<=this.getWidth(); X++){
 				// Do the work per-pixel here
 				ComplexNumber c1 = getComplex(X, Y);
 				ComplexNumber c2 = new ComplexNumber(c1);
 				// Calculate divergence up to ITTERATIONS
 				int n = 0;
 				while(n<=ITTERATIONS){
-					// Do the formula: Z(i+1) = (Z(i) * Z(i)) + c. Where c = c1 and Z(i) = c2;
-					c2 = c2.square().add(c1);
 					// Escape if it does diverge
 					if(c2.modulusSquared() >= 4){
 						break;
 					}
+					// Do the formula: Z(i+1) = (Z(i) * Z(i)) + c. Where c = c1 and Z(i) = c2;
+					c2 = c2.square().add(c1);
 					n++;
 				}
 				// Paint the on the pixel according to the iterations
@@ -60,14 +60,14 @@ public class Mandelbrot extends JPanel{
 	// Helper, takes point and finds it's complex number
 	private ComplexNumber getComplex(int x, int y){
 		// Represents the how much on the r-i axis to move per pixel
-		double stepX = (maxX-minX)/this.getWidth();
-		double stepY = (maxY-minY)/this.getHeight();
+		double stepX = (maxR-minR)/this.getWidth();
+		double stepY = (maxI-minI)/this.getHeight();
 		
 		// Move the coordinates to the right place in the r-t axis
 		double real = x*stepX;
 		double imaginary = y*stepY;
-		real = minX + real;
-		imaginary = maxY - imaginary;
+		real = minR + real;
+		imaginary = maxI - imaginary;
 		
 		//if(real <= maxX && imaginary >= minY){
 			return new ComplexNumber(real, imaginary);
@@ -81,21 +81,23 @@ public class Mandelbrot extends JPanel{
 	// TODO: create a better algorithm
 	private Color genColor(int it){
 		
+		
+		/*
 		if(it <= 10) return Color.YELLOW;
-		if(it <= 20) return Color.LIGHT_GRAY;
-		if(it <= 30) return Color.PINK;
+		//if(it <= 20) return Color.LIGHT_GRAY;
+		//if(it <= 30) return Color.PINK;
 		if(it <= 40) return Color.MAGENTA;
-		if(it <= 50) return Color.GREEN;
+		//if(it <= 50) return Color.GREEN;
 		
-		if(it <= 60) return Color.BLUE;
-		if(it <= 70) return Color.CYAN;
+		//if(it <= 60) return Color.BLUE;
+		//if(it <= 70) return Color.CYAN;
 		if(it <= 80) return Color.RED;
-		if(it <= 90) return Color.ORANGE;
+		//if(it <= 90) return Color.ORANGE;
+		*/
 		
 		
-		
-		if(it >= 100) return Color.BLACK;
-		return null;
+		if(it >= ITTERATIONS) return Color.BLACK;
+		return Color.WHITE;
 	}
 	
 	
@@ -114,8 +116,8 @@ public class Mandelbrot extends JPanel{
 	// Helper function takes a complex number and converts it into a point on the normalised axis
 	private Point getPoint(ComplexNumber c){
 		// Represents the number per px on the screen
-		double stepX = (maxX-minX)/this.getWidth();
-		double stepY = (maxY-minY)/this.getHeight();
+		double stepX = (maxR-minR)/this.getWidth();
+		double stepY = (maxI-minI)/this.getHeight();
 		
 		
 		
